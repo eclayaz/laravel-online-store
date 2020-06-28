db18c5ac568a6a0a1d8377456c9665c7114c1e99
Nadeesha Dilruwan <eclayaz@gmail.com>
2020-06-28 11:23:42 +0530

    add csv files

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,23 @@ class ProductController extends Controller
 {
     public function index()
     {
-        return 'dddd';
+//        $file = public_path('files/test.csv');
+//        var_dump($file);
+
+        $csvFileName = "product.csv";
+        $csvFile = storage_path('app/csv/' . $csvFileName);
+        $dd = $this->readCSV($csvFile,array('delimiter' => ','));
+
+        return $dd;
+    }
+
+    public function readCSV($csvFile, $array)
+    {
+        $file_handle = fopen($csvFile, 'r');
+        while (!feof($file_handle)) {
+            $line_of_text[] = fgetcsv($file_handle, 0, $array['delimiter']);
+        }
+        fclose($file_handle);
+        return $line_of_text;
     }
 }
